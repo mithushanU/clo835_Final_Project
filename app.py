fd9d529e810f555cfc782b55fb23232f2007d1b0
Mithushan Uthayakumar <mithushanuthayakumar@gmail.com>
2024-07-31 18:40:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,144 +1,57 @@
-from flask import Flask, render_template, request
-from pymysql import connections
+from flask import Flask
+from flask import render_template, url_for
+import socket
+import mysql.connector
 import os
-import random
-import argparse
+import boto3
+import io
 
 app = Flask(__name__)
 
-DBHOST = os.environ.get("DBHOST") or "localhost"
-DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "password"
-DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT") or 3306)
+DB_Host = os.environ.get('DB_Host') or "mysql"
+DB_Database = os.environ.get('DB_Database') or "mysql"
+DB_User = os.environ.get('DB_User')
+DB_Password = os.environ.get('DB_Password') 
+groupname = os.environ.get("GROUP_NAME")
+image_uri = os.environ.get("Image_Uri")
+AWS_REGION = os.environ.get("AWS_REGION")
+S3_BUCKET_NAME= os.environ.get("S3_BUCKET_NAME")
 
-# Create a connection to the MySQL database
-db_conn = connections.Connection(
-    host=DBHOST,
-    port=DBPORT,
-    user=DBUSER,
-    password=DBPWD,
-    db=DATABASE
-)
-output = {}
-table = 'employee'
+path = '/clo835/config/Image_Uri/'+image_uri
 
-# Define the supported color codes
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#89CFF0",
-    "blue2": "#30336b",
-    "pink": "#f4c2c2",
-    "darkblue": "#130f40",
-    "lime": "#C1FF9C",
-}
+basename = os.path.basename(path)
 
-# Create a string of supported colors
-SUPPORTED_COLORS = ",".join(color_codes.keys())
+# print(basename)
 
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
 
-@app.route("/app1", methods=['GET', 'POST'])
-def home1():
-    return render_template('addemp.html', color=color_codes[COLOR])
+s3_resource = boto3.resource("s3", region_name=AWS_REGION)
 
-@app.route("/app2", methods=['GET', 'POST'])
-def home2():
-    return render_template('addemp.html', color=color_codes[COLOR])
+s3_object = s3_resource.Object(S3_BUCKET_NAME, basename)
 
-@app.route("/app3", methods=['GET', 'POST'])
-def home3():
-    return render_template('addemp.html', color=color_codes[COLOR])
+s3_object.download_file('static/success.jpg') 
 
-@app.route("/about", methods=['GET', 'POST'])
-def about():
-    return render_template('about.html', color=color_codes[COLOR])
+# print('S3 object download complete')
+# print("groupname", groupname)
 
-@app.route("/addemp", methods=['POST'])
-def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    primary_skill = request.form['primary_skill']
-    location = request.form['location']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-
+@app.route("/")
+def main():
+    db_connect_result = False
+    err_message = ""
     try:
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, primary_skill, location))
-        db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-
-    finally:
-        cursor.close()
-
-    print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
-
-@app.route("/getemp", methods=['GET', 'POST'])
-def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
-
-@app.route("/fetchdata", methods=['GET', 'POST'])
-def FetchData():
-    emp_id = request.form['emp_id']
-
-    output = {}
-    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id=%s"
-    cursor = db_conn.cursor()
-
-    try:
-        cursor.execute(select_sql, (emp_id,))
-        result = cursor.fetchone()
-
-        if result:
-            output["emp_id"] = result[0]
-            output["first_name"] = result[1]
-            output["last_name"] = result[2]
-            output["primary_skills"] = result[3]
-            output["location"] = result[4]
-        else:
-            return "Employee not found", 404
-
+        mysql.connector.connect(host=DB_Host, database=DB_Database, user=DB_User, password=DB_Password)
+        color = '#39b54b'
+        db_connect_result = True
     except Exception as e:
-        print(e)
-        return str(e), 500
+        color = '#ff3f3f'
+        err_message = str(e)
 
-    finally:
-        cursor.close()
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, image_uri=image_uri, groupname=groupname)
 
-    return render_template("getempoutput.html", id=output.get("emp_id"), fname=output.get("first_name"),
-                           lname=output.get("last_name"), interest=output.get("primary_skills"), location=output.get("location"), color=color_codes[COLOR])
+@app.route("/debug")
+def debug():
+    color = '#2196f3'
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color)
 
-if __name__ == '__main__':
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV +
-                  ". However, color from command line argument takes precedence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
-    app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81)
