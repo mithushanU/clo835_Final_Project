d9ad65a128760d5ea623d7d03e8b7acafa600528
Mithushan Uthayakumar <mithushanuthayakumar@gmail.com>
2024-08-01 09:37:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def download_image_from_s3():
 
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "passwors"
+DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
